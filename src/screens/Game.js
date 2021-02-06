c7a4fa91642a8f6b3b22e784087ed50673c6c2bb
Trong Tran <trong.tran@mservice.com.vn>
2021-02-06 12:43:20 +0700

    Commit changes

---
+++ src/screens/Game.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import PropTypes from 'prop-types'
+export default class Game extends Component {
+
+    
+
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )
+    }
+}
